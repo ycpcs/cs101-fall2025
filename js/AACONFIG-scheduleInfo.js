30f927cc9c34d48dc765cd9ade3bdab333bfb9f8
David Babcock <dbabcock@ycp.edu>
2025-08-26 10:05:53 -0400

    update final exams
    update final exams

--- js/AACONFIG-scheduleInfo.js
+++ js/AACONFIG-scheduleInfo.js
@@ -173,9 +173,8 @@ courseInfo.classPeriods = [
 // The following is for the college-scheduled final exam.
 // It is not used if final is on last day of class"
 courseInfo.finalExamDates = [
-		new FinalExamDay("103 (11:00am)", new Date("12/12/2024 10:15:00")),
-		new FinalExamDay("101 (2:00pm)", new Date("12/12/2024 12:45:00")),
-		new FinalExamDay("102 (9:30am)", new Date("12/14/2024 08:00:00")),
+		new FinalExamDay("102 (11:00am)", new Date("12/11/2025 10:15:00")),
+		new FinalExamDay("103 (5:00pm)", new Date("12/11/2024 17:00:00")),
 ];
 
 // vim:ts=2:
